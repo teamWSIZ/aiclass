2cd6e815204cc2d89d76f1e5e4894b5dc348dff4
Piotr Marecki <pmarecki.pro@gmail.com>
2021-06-09 15:53:06 +0200

    wstępne poprawianie wartości ruchów

--- reinforcement_learning/tic_tac_toe/net1.py
+++ reinforcement_learning/tic_tac_toe/net1.py
@@ -1,9 +1,11 @@
+from random import choice
+
 import torch
 from torch import nn, optim
 import torch.nn.functional as funct
 
 # https://pytorch.org/tutorials/beginner/saving_loading_models.html
-from reinforcement_learning.tic_tac_toe.ttt_utils import apply_move, valid_moves, is_winning
+from reinforcement_learning.tic_tac_toe.ttt_utils import apply_move, valid_moves, is_winning, print_state, random_state
 from supervised.helper import *
 
 
@@ -39,15 +41,15 @@ class TicTacToeNet(nn.Module):
 dtype = torch.double
 # device = 'cpu'  # gdzie wykonywać obliczenia
 device = 'cpu'
-IN_SZ = 18  # ile liczb wchodzi (długość listy)
+IN_SIZE = 18  # ile liczb wchodzi (długość listy)
 HID = 5  # ile neuronów w warstwie ukrytej
-OUT_SZ = 9  # proponowane ruchy dla "cross" (trzeba sprawdzić czy są "valid")
+OUT_SIZE = 9  # proponowane ruchy dla "cross" (trzeba sprawdzić czy są "valid")
 
 EPOCHS = 1000
 LR = 0.01
 
 # Net creation
-net = TicTacToeNet(IN_SZ, HID, OUT_SZ)
+net = TicTacToeNet(IN_SIZE, HID, OUT_SIZE)
 net = net.double()
 # net.load('saves/one.dat')
 
@@ -86,32 +88,71 @@ if device == 'cuda':
 loss_function = nn.MSELoss(reduction='mean')
 optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9)  # będzie na GPU, jeśli gpu=True
 
-s = [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0]
+# todo !!!!!
+s = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0]
 ts = tensor(s, dtype=dtype)
 prediction = net(ts)
-print(prediction)
+print_state(s)
+print('początkowa predykcja:', prediction)
 pd = prediction.data
-print(pd.tolist()[0])
+
+
+# print(pd.tolist()[0])
 
 
 def expected_reward(state, ax, net: TicTacToeNet, discount_lambda):
     """
+    Funkcja podająca spodziewaną nagrodę "o 1 ruch dalej" ...
+    Funkcja ocenia wyniky wykonania ruchu "ax" (przez "x") w stanie "state"
+
     Zakładamy, że "o" wybierze taki ruch, by zminimalizować prognozowany przez `net` reward następnej
     pozycji "x"-a.
     :return:
     """
     nstate_o = apply_move(state, ax)
-    if is_winning(nstate_o):
+    if is_winning(nstate_o[9:]):
+        return -100  # nawet przed ruchem pozycja była przegrana
+    if is_winning(nstate_o[:9]):
+        # nasz ruch (x-em) doprowadził do zwycięstwa --> 100pkt
         return 100
-    valid_o_moves = valid_moves(nstate_o, cross=False)
+
+    valid_o_moves = valid_moves(nstate_o, cross=False)  # wszystkie ruchy gracza "o"
     best_for_o = 100
     for ao in valid_o_moves:
         nstate_x = apply_move(nstate_o, ao)
         if is_winning(nstate_x[9:]):
-            return -100
-        best_for_x = max(net(nstate_x)[0].tolist())
+            return -100  # kółko ma ruch wygrywający, czyli nagroda dla "x" za ruch "ax" w stanie "state" jest -100
+        nstate_x_tensor = tensor(nstate_x, dtype=dtype, device=device)
+
+        # predykcja wartości stanu "nstate_x" przez akutalną sieć neuronową
+        best_for_x = max(net(nstate_x_tensor)[0].tolist())
+
+        # o szuka stanu w którym najlepszy ruch dla x daje x-owi jak najmniej
         best_for_o = min(best_for_o, best_for_x)
     return discount_lambda * best_for_o
+
+
+# valid_x_moves = valid_moves(s, cross=True)  # dostępne ruchy dla "x"
+# for m in valid_x_moves:
+#     print_state(apply_move(s, m))
+#     print('"lepsza" predykcja wartości dla tego ruchu: ', expected_reward(s, m, net, 0.9))
+#     print('---')
+
+for i in range(15):
+    s = random_state(3, 3)
+    print_state(s)
+    st = tensor(s, dtype=dtype, device=device)
+    current = net(st)[0]
+    print('predykcja początkowa:', current.tolist())
+    m = choice(valid_moves(s, cross=True))  # losowy ruch
+    er = expected_reward(s, m, net, 0.9)
+    print(net(torch.cat((st,st,st))))
+    print(f'"lepsza" predykcja wartości dla ruchu: {m}', er)
+    at = m.index(1)
+    current[at] = er
+    print(f'predykcja aktualna  : {current.tolist()}, at={at}')
+    # print(f's={s} updated:{}')
+    print('---------------')
 
 # Training
 # for epoch in range(EPOCHS):
--- reinforcement_learning/tic_tac_toe/ttt_utils.py
+++ reinforcement_learning/tic_tac_toe/ttt_utils.py
@@ -1,24 +1,31 @@
+from random import randint
 from typing import List
 
 
 def is_winning(state: List[int]):
     """
-    :param state: płaska reprezentacja planszy 3x3 (wszystkie liczby zebrane do pojedynczej listy)
+    :param state: płaska reprezentacja planszy 3x3 (wszystkie liczby zebrane do pojedynczej listy), lista na 9 elementów
     :return: True jeśli pozycja wygrywa w TTT, False jeśli nie wygrywa (może być "otwarta")
     """
     if state[0:3] == [1] * 3 or state[3:6] == [1] * 3 or state[6:9] == [1] * 3:
-        return True
+        return True  # sprawdzenie rzędów
     for c in range(3):
+        # sprawdzenie kolumny nr "c"
         if state[c] == 1 and state[c + 3] == 1 and state[c + 6] == 1:
             return True
+    # przekątna
     if state[0] == 1 and state[4] == 1 and state[8] == 1:
         return True
+    # "anty-przekątna"
     if state[2] == 1 and state[4] == 1 and state[6] == 1:
         return True
     return False
 
 
 def print_state(state: List[int]):
+    """
+    Drukuje planszę z pionkami; `state` to lista z 18 elementami
+    """
     board = ['.'] * 9
     for i in range(9):
         if state[i] == 1:
@@ -31,9 +38,16 @@ def print_state(state: List[int]):
 
 def valid_moves(state: List[int], cross=True):
     """
+    Ruch to tablica 18 elementów; pierwsze 9 odpowiadają ruchom "x"-a; pozostałe ruchom "o"-ka;
+    w całej tablicy jest tylko jedna 1; pozostałe to 0.
+
+    Np. ruch w rzędzie 1 (numerowane od 0), i kolumnie 0 "x"-a ma postać:
+    [0,0,0,1,0,0,0,0,0, 0,0,0,0,0,0,0,0,0]
+
     :param state: obecny stan planszy
     :param cross: czy szukamy ruchów dla "x" (False → "o")
     :return: tablica dostępnych ruchów (tablic 18-elementowych)
+
     """
     moves = []
     offset = 0 if cross else 9
@@ -70,15 +84,35 @@ def best_moves(state, qvalues):
     return moves
 
 
+
+def random_state(n_cross, n_circle):
+    s = [0] * 18
+
+    # losowanie "x"-ów
+    while sum(s) < n_cross:
+        pos = randint(0, 8)
+        s[pos] = 1
+
+    # losowanie "o"-ek
+    while sum(s) < n_cross + n_circle:
+        pos = randint(9, 17)
+        if s[pos - 9] == 0:
+            s[pos] = 1
+
+    return s
+
 if __name__ == '__main__':
-    print(is_winning([1, 1, 1, 0, 0, 0, 0, 0, 0]), True)
-    print(is_winning([1, 1, 0, 0, 0, 0, 0, 0, 0]), False)
-    print(is_winning([1, 1, 0, 1, 0, 0, 1, 0, 0]), True)
+    # print(is_winning([1, 1, 1, 0, 0, 0, 0, 0, 0]), True)
+    # print(is_winning([1, 1, 0, 0, 0, 0, 0, 0, 0]), False)
+    # print(is_winning([1, 1, 0, 1, 0, 0, 1, 0, 0]), True)
     print(is_winning([1, 0, 0, 0, 1, 0, 1, 0, 1]), True)
-    print_state([1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0])
-    s = [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0]
+    s = [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0]
+    print_state(s)
+    # move = [0,0,0,0,0,0,0,0,0, 0,0,0,1,0,0,0,0,0]
+    # nstate = apply_move(s, move)
+    # print(nstate)
+    # print_state(nstate)
     next_moves = valid_moves(s, cross=False)
     for m in next_moves:
         print('---')
         print_state(apply_move(s, m))
-
